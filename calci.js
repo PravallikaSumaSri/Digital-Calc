e36561d2b771ce38708bac8e561b08baed96e277
PravallikaSumaSri <138872815+PravallikaSumaSri@users.noreply.github.com>
2023-07-07 22:49:58 +0530

    Add files via upload

---
+++ calci.js
@@ -0,0 +1,18 @@
+var clickSound=new Audio()
+clickSound.src="./sol-101774.mp3"
+function clickAudio()
+{
+    clickSound.play()
+}
+var clickSound1=new Audio()
+clickSound1.src="./toy-piano-87353.mp3"
+function clickAudio1()
+{
+    clickSound1.play()
+}
+var clickSound2=new Audio()
+clickSound2.src="./piano-g-6200.mp3"
+function clickAudio2()
+{
+    clickSound2.play()
+}
